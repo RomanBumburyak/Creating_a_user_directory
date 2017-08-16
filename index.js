f03a785403318f78e9a0c82eb3832e0be09040bb
Roman <roman@romans-mbp.lan>
2017-08-16 09:12:45 -0400

    second commit

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.get("/Hello", function(req, res){
   res.render('users', data)
 });
+app.get('listing/:id' , function(req, res){
+  console.log(req.params.id);
+})
 
 app.listen(8080, function(){
   console.log("App is running on port 8080");
