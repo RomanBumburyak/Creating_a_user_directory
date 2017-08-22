839deab7ec4cfa476f6cba86301b3cb30201dab9
Roman <roman@romans-mbp.lan>
2017-08-22 13:21:30 -0400

    Initial commit

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const path = require('path');
 const mustacheExpress = require('mustache-express');
-const data = require('./data')
 const app = express();
+const userRoutes = require('./');
+const Data = require('./models/data.js');
+const router = require('./routes/user.js');
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.get("/Hello", function(req, res){
-  res.render('users', data)
-});
-app.get('listing/:id' , function(req, res){
-  console.log(req.params.id);
-})
+
+app.get('/', function (req,res) {
+  res.render('index', {userList: Data.allusers});
+)};
+
 
 app.listen(8080, function(){
   console.log("App is running on port 8080");
